cc5521f55b29cb4d23dee0a4f167c04c630a95f1
bMinah <rattanajoey@gmail.com>
2018-03-14 22:52:14 -0400

    Main Screen View Controller w/ constraints.

---
+++ YourWorld/YourWorld/Controller/Register2ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  Register2ViewController.swift
+//  YourWorld
+//
+//  Created by Joey Rattana on 3/14/18.
+//  Copyright © 2018 Joey Rattana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Register2ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
