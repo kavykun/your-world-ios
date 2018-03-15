bb69da397b49ac27820e699e45d8c686c01cfc68
bMinah <rattanajoey@gmail.com>
2018-03-14 22:26:15 -0400

    Welcome, Login, Register View Controllers. //No constraints yet

---
+++ YourWorld/YourWorld/Controller/RegisterViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RegisterViewController.swift
+//  YourWorld
+//
+//  Created by Joey Rattana on 3/14/18.
+//  Copyright © 2018 Joey Rattana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
