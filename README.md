0c9d14a6785725d535db64c31217e3023626912e
Kavy Rattana <kavyrattana@gmail.com>
2018-07-15 20:18:30 -0700

    Add apollo connector

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # your-world-ios
+
+apollo-codegen introspect-schema http://localhost:4000 --output schema.json
--- YourWorld/Controller/LogInViewController.swift
+++ YourWorld/Controller/LogInViewController.swift
@@ -8,12 +8,17 @@
 
 import Foundation
 import UIKit
+import Apollo
+
+let graphlQLEndpointURL = "http://localhost:4000"
+let apollo = ApolloClient(url: URL(string: graphlQLEndpointURL)!)
 
 class LogInViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        fetchPosts()
     }
     
     override func didReceiveMemoryWarning() {
@@ -21,5 +26,28 @@ class LogInViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    var posts: PostsQuery.Data.Feed? {
+        didSet {
+            print("update view here")
+        }
+    }
+    
+    func fetchPosts() {
+        let postsQuery = PostsQuery()
+        apollo.fetch(query: postsQuery) { result, error in
+            if let error = error {
+                print(#function, "ERROR | An error occured: \(error)")
+                return
+            }
+            guard let posts = result?.data?.feed.posts else {
+                print(#function, "ERROR | Could not retrieve posts")
+                return
+            }
+            
+            for post in posts {
+                print(post.title)
+            }
+        }
+    }
     
 }
