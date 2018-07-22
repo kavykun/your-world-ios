638512d343c8faea5d6da2719b3ca679f98e7a56
bMinah <rattanajoey@gmail.com>
2018-07-22 15:30:39 -0400

    autolayout fixed for button and delegation fixed for mainscreencontroller

--- YourWorld/Controller/MainScreenViewController.swift
+++ YourWorld/Controller/MainScreenViewController.swift
@@ -6,20 +6,53 @@
 //  Copyright © 2018 Joey Rattana. All rights reserved.
 //
 
-import Foundation
 import UIKit
+import MapKit
+import CoreLocation
+
 
 class MainScreenViewController: UIViewController {
     
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var targets = [LocationItem]()
+    var userLocation: CLLocation?
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.userTrackingMode = MKUserTrackingMode.followWithHeading
+        
+        func setupLocations() {
+            let firstTarget = LocationItem(itemDescription: "location 1", location: CLLocation(latitude: 36.0212784, longitude: -80.00454839999998))
+            targets.append(firstTarget)
+            
+            let secondTarget = LocationItem(itemDescription: "location 2", location: CLLocation(latitude: 36.0213905, longitude: -80.0052574))
+            targets.append(secondTarget)
+            
+            let thirdTarget = LocationItem(itemDescription: "location 3", location: CLLocation(latitude: 36.0221618, longitude: -80.00529640000002))
+            targets.append(thirdTarget)
+            
+            for item in targets {
+                let annotation = MapAnnotation(location: item.location.coordinate, item: item)
+                self.mapView.addAnnotation(annotation)
+            }
+        }
+        
+        setupLocations()
+        
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension MainScreenViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        self.userLocation = userLocation.location
     }
-    
-    
 }
